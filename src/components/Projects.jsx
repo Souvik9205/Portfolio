99d10c132bc56f37dffd102c7b2bdb47fc250cf2
Souvik9205 <souvik9205@gmail.com>
2023-12-26 23:26:02 +0530

    icons and projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,28 +13,28 @@ const Projects = () => {
                         Created by a beginner exploring web development, this app is a product of practice and learning. It's built using the MERN stack and incorporates various cutting-edge technologies. It's a testament to growth and experimentation in the world of web development
                     </p>
                 </div>
-                <div className="flex flex-wrap -m-4">
+                <div className="flex flex-wrap m-4 aspect-w-16 aspect-h-9">
                     {projects.map((project) => (
                         <a
-                            href={project.link}
-                            key={project.image}
-                            className="sm:w-1/2 w-full p-4"
-                            >
-                            <div className="relative overflow-hidden rounded-lg">
-                                <div
-                                className="aspect-w-16 aspect-h-9 bg-gray-900"
-                                style={{ backgroundImage: `url(${project.image})`, backgroundPosition: 'center', backgroundSize: 'cover' }}
-                                ></div>
-                                <div className="p-6 bg-gray-900 opacity-0 hover:opacity-100">
-                                <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                                    {project.subtitle}
-                                </h2>
-                                <h1 className="title-font text-lg font-medium text-white mb-3">
-                                    {project.title}
-                                </h1>
-                                <p className="leading-relaxed">{project.description}</p>
-                                </div>
+                        href={project.link}
+                        key={project.image}
+                        className="sm:w-1/2 w-100 p-4">
+                        <div className="flex relative">
+                            <img
+                            alt="gallery"
+                            className="absolute inset-0 w-full h-full object-cover object-center"
+                            src={project.image}
+                            />
+                            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                            <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                                {project.subtitle}
+                            </h2>
+                            <h1 className="title-font text-lg font-medium text-white mb-3">
+                                {project.title}
+                            </h1>
+                            <p className="leading-relaxed">{project.description}</p>
                             </div>
+                        </div>
                         </a>
                     ))}
                 </div>
