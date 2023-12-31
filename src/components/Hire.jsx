dff9f842ddeec56bb02c71badd3ce4311a4e03dd
Souvik9205 <souvik9205@gmail.com>
2023-12-31 09:13:16 +0530

    hire update again

--- src/components/Hire.jsx
+++ src/components/Hire.jsx
@@ -16,8 +16,13 @@ const Hire = () => {
       function handleSubmit(e) {
         e.preventDefault();
 
-        const myForm = e.target;
-        const formData = new FormData(myForm);
+        const formData = {
+            "form-name" : "contact",
+            name,
+            email,
+            message,
+        };
+
         fetch("/", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -65,8 +70,9 @@ const Hire = () => {
                 </div>
             </div>
             <form
-                netlify
+                data-netlify="true"
                 name="contact"
+                method="POST"
                 onSubmit={handleSubmit}
                 className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                 <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
